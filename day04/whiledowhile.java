b3af1da136dfd2c4898207e0e81872445dc3a138
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-06-03 12:48:53 +0530

    Add files via upload

---
+++ day04/whiledowhile.java
@@ -0,0 +1,16 @@
+public class whiledowhile {
+    public static void main(String[] args) {
+        System.out.println("Day 04 ");
+        int x=0;
+       while(x<5)                                        //while loop
+       {
+            x+=1;
+            System.out.println("n = "+x);
+       }
+       do{                                                //do while loop
+            x+=1;
+            System.out.println("n = "+x);
+          
+        }while(x<6);  
+    }
+}
