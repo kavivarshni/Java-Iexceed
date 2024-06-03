c5895cf70c32b64bece671c4acee1408b74beda4
Kavivarshni M <87589645+kavivarshni@users.noreply.github.com>
2024-06-03 12:05:42 +0530

    Add files via upload

---
+++ day01/relationalOperators.java
@@ -0,0 +1,15 @@
+public class relationalOperators {
+    public static void main(String[] args) {
+        int x=10,y=20;
+        System.out.println("  Relational Operators");      
+        System.out.println(x<y);
+        System.out.println(x<=y);
+         System.out.println(x>y);
+        System.out.println(x>=y);
+        System.out.println(x==y);
+        System.out.println(x!=y);
+        
+        System.out.println(" Logical Operators");
+         System.out.println((x>y) && (x==y));
+    }
+}
